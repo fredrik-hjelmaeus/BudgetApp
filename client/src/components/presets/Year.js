5565d13af2dbc6dad2a4a32d71baf4b1dc84dded
KirderfOvane <gemigpost@hotmail.com>
2020-04-10 18:56:18 +0200

    swipe2test

--- client/src/components/presets/Year.js
+++ client/src/components/presets/Year.js
@@ -29,7 +29,7 @@ const Year = () => {
     calcCategorySumOnlyPosNumByYear,
     calcCategorySumOnlyNegNumByYear,
     setCategoryNameOnlyNegNumByYear,
-    calcAllMonthSum
+    calcAllMonthSum,
   } = presetContext;
 
   const { yearsummary, dimensions } = cssContext;
@@ -66,11 +66,12 @@ const Year = () => {
         'September',
         'October',
         'November',
-        'December'
+        'December',
       ]);
     // eslint-disable-next-line
   }, [presets, year]);
 
+  const [cssp1display, setCssp1display] = useState('block');
   const [cssp2display, setCssp2display] = useState('none');
   const [startingX, setStartingX] = useState('');
   const [movingX, setMovingX] = useState('');
@@ -79,13 +80,13 @@ const Year = () => {
   let cssLeftChange = 0;
   let cssPage2LeftChange = '100%';
   //let cssp2display;
-  const onTouchStart = e => {
+  const onTouchStart = (e) => {
     setStartingX(e.touches[0].clientX);
     setCssp2display('block');
     // console.log(`startingX:${startingX}`);
   };
 
-  const onTouchMove = e => {
+  const onTouchMove = (e) => {
     setMovingX(e.touches[0].clientX);
     change = parseFloat(startingX) - parseFloat(movingX);
     cssLeftChange = `-${change}px`;
@@ -96,27 +97,46 @@ const Year = () => {
     if (change < 0) {
       return;
     }
-    console.log(movingX);
+    //console.log(movingX);
   };
+  // screenX = 2474-2194 = 280     touchend = 2474-screenX * (100/224)
+  //screen width 375     touchstart = startingX * (100/375)
+  const onTouchEnd = (e) => {
+    //change = parseFloat(startingX) - parseFloat(e.changedTouches[0].clientX);
 
-  const onTouchEnd = e => {
-    change = parseFloat(startingX) - parseFloat(e.changedTouches[0].clientX);
+    let touchend = parseFloat(2474) - parseFloat(e.changedTouches[0].screenX);
+    touchend = touchend * (100 / 280);
+    touchend = parseFloat(100) - parseFloat(touchend);
+    console.log(`touchend: ${touchend} %`);
 
-    const threshold = window.screen.width / 3;
+    let touchstart = parseFloat(startingX) * parseFloat(100 / 375);
+    console.log(`touchstart: ${touchstart} %`);
+
+    //percentchange
+    let change = parseFloat(touchstart) - parseFloat(touchend);
+    //console.log(change);
+
+    //console.log(e.changedTouches[0].screenX);
+    // console.log(window.screen.width);
+
+    const threshold = 33;
     if (change < threshold) {
       setCssp2display('none');
-      cssLeftChange = 0;
+      setCssp1display('block');
+      cssLeftChange = '-100%';
       cssPage2LeftChange = '100%';
     } else {
       setCssp2display('block');
-      cssLeftChange = '-100%';
+      setCssp1display('none');
+      cssLeftChange = '100%';
       cssPage2LeftChange = '0';
     }
-    console.log(`startingX:${startingX}`);
+    // console.log(`startingX:${startingX}`);
     console.log(`Change:${change}`);
-    // console.log(`threshold: ${window.screen.width / 3}`);
-    console.log(`changedtouches: ${e.changedTouches[0].clientX}`);
-    console.log(e.changedTouches[0].clientX);
+    //console.log(`threshold: ${window.screen.width / 3}`);
+    //console.log(`changedtouches: ${e.changedTouches[0].clientX}`);
+    //console.log(e.changedTouches[0].clientX);
+    // console.log(movingX);
   };
 
   return (
@@ -126,7 +146,7 @@ const Year = () => {
         onTouchStart={onTouchStart}
         onTouchMove={onTouchMove}
         onTouchEnd={onTouchEnd}
-        style={{ left: cssLeftChange }}
+        style={{ left: cssLeftChange, display: cssp1display }}
       >
         Page1
       </div>
