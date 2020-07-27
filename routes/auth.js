6db0380d3797444937811e8260c75d754cf0a445
KirderfOvane <gemigpost@hotmail.com>
2020-07-27 14:02:08 +0200

    forgot and reset password implemented

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
 const auth = require('../middleware/auth');
+const sendEmail = require('../utils/sendEmail');
 
 const User = require('../models/User');
 
@@ -83,5 +85,79 @@ router.post(
     }
   }
 );
+
+// @route   POST api/auth/forgotpassword
+// @desc    Forgot password
+// @access  Public
+router.post('/forgotpassword', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({ data: 'There is no user with that email' });
+    }
+
+    // Get reset token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    // Create reset url
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/auth/forgotpassword/${resetToken}`;
+
+    const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+    try {
+      await sendEmail({
+        email: user.email,
+        subject: 'Password reset token',
+        message,
+      });
+
+      res.status(200).json({ success: true, data: 'Email sent' });
+    } catch (err) {
+      console.log(err);
+      user.resetPasswordToken = undefined;
+      user.resetPasswordExpire = undefined;
+
+      await user.save({ validateBeforeSave: false });
+
+      return res.status(500).json({ data: 'Email could not be sent' });
+    }
+
+    res.status(200).json({ msg: 'Email sent, check your mailbox', data: user });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @desc          Reset Password
+// @route         PUT /api/auth/resetpassword/:resettoken
+// @access        Public
+router.put('/resetpassword/:resettoken', async (req, res) => {
+  try {
+    // Get hashed token
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.resettoken).digest('hex');
+
+    const user = await User.findOne({ resetPasswordToken, resetPasswordExpire: { $gt: Date.now() } });
+
+    if (!user) {
+      return res.status(400).json({ data: 'Invalid token' });
+    }
+
+    // Set/Encrypt new password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(req.body.password, salt);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    res.status(200).json({ data: 'Password Changed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
---
+++ utils/sendEmail.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer');
+const config = require('config');
+
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: config.get('SMTP_HOST'),
+    port: config.get('SMTP_PORT'),
+    auth: {
+      user: config.get('SMTP_EMAIL'),
+      pass: config.get('SMTP_PASSWORD'),
+    },
+  });
+
+  //  define transport object
+  const message = {
+    from: `${config.get('FROM_NAME')} <${config.get('FROM_EMAIL')}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+  const info = await transporter.sendMail(message);
+
+  console.log('Message sent: %s', info.messageId);
+};
+
+module.exports = sendEmail;
