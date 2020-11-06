e52023b7bb1e04005eb954283a9f3f4cde8d3331
K <gemigpost@hotmail.com>
2020-11-06 18:21:06 +0100

    helmet,sanitize,xss,ratelimit,hpp security implemented

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const connectDB = require('./config/db');
 const app = express();
 const path = require('path');
 const fileUpload = require('express-fileupload');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 
 // Connect Database
 connectDB();
@@ -10,6 +15,25 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 app.use(fileUpload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 300, //num of request allowed
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
