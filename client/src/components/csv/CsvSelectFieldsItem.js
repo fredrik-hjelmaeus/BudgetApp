e74a4eedd103bcd13a250160e43996db20d88632
K <gemigpost@hotmail.com>
2020-11-21 15:05:20 +0100

    css selectCsvfields

--- client/src/components/csv/CsvSelectFieldsItem.js
+++ client/src/components/csv/CsvSelectFieldsItem.js
@@ -5,22 +5,12 @@ import FirstCsvRow from './FirstCsvRow';
 const CsvSelectFieldsItem = ({ header, rowItem, fieldSelect }) => {
   const { row } = rowItem;
 
-  return header ? (
-    <React.Fragment>
-      <div className='flexrow__csvrow'>
-        {Object.keys(row).map((col) => (
-          <FirstCsvRow col={col} fieldSelect={fieldSelect} />
-        ))}
-      </div>
-    </React.Fragment>
-  ) : (
-    <React.Fragment>
-      <div className='flexrow__csvrow'>
-        {Object.keys(row).map((col) => (
-          <CsvRows col={col} row={row} />
-        ))}
-      </div>
-    </React.Fragment>
+  return (
+    <div className='CsvSelectFieldsItem__flexrow'>
+      {header
+        ? Object.keys(row).map((col) => <FirstCsvRow col={col} fieldSelect={fieldSelect} />)
+        : Object.keys(row).map((col) => <CsvRows col={col} row={row} />)}
+    </div>
   );
 };
 export default CsvSelectFieldsItem;
