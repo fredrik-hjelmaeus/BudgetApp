714d381f3e22f96cbdc371b4c95c3ca92862e60a
K <gemigpost@hotmail.com>
2020-12-24 14:57:10 +0100

    guide css

--- client/src/components/guide/GuideSteps.js
+++ client/src/components/guide/GuideSteps.js
@@ -4,12 +4,16 @@ import DotStepsMenu from './DotStepsMenu';
 const GuideSteps = ({ text, setGuide, guide, nextStep, onExit }) => {
   return (
     <>
-      <div className='guide__datemenustep'>
+      <div className='guide__card'>
         <DotStepsMenu guide={guide} setGuide={setGuide} />
         <div>{text}</div>
-        <div>
-          <button onClick={onExit}>Exit Guide</button>
-          <button onClick={nextStep}>Next</button>
+        <div className='guide__btn__group'>
+          <button className='guide__btn__group__next' onClick={nextStep}>
+            Next
+          </button>
+          <button className='guide__btn__group__exit' onClick={onExit}>
+            Exit
+          </button>
         </div>
       </div>
     </>
