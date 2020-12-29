011bd0bcdd0342b2c9f31a355188c864c2627b2b
K <gemigpost@hotmail.com>
2020-12-29 16:40:53 +0100

    guide mobile adaptation ongoing

--- client/src/components/guide/GuideSteps.js
+++ client/src/components/guide/GuideSteps.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import DotStepsMenu from './DotStepsMenu';
 
-const GuideSteps = ({ text, setGuide, guide, nextStep, onExit }) => {
+const GuideSteps = ({ text, setGuide, guide, nextStep, onExit, prevStep }) => {
   return (
     <>
-      <div className='guide__card'>
+      <div
+        className={
+          !isNaN(guide) && parseInt(guide) >= 6 && parseInt(guide) < 10 ? 'guide__card guide__card__addtobudgetsteps' : 'guide__card'
+        }
+      >
         <DotStepsMenu guide={guide} setGuide={setGuide} />
+        <button className='guide__closebtn' value='close' onClick={onExit}></button>
         <div className='guide__text'>{text}</div>
         <div className='guide__btn__group'>
+          {guide !== '2' && (
+            <button className='guide__btn__group__prev' onClick={prevStep}>
+              Previous
+            </button>
+          )}
           {nextStep && (
             <button className='guide__btn__group__next' onClick={nextStep}>
               Next
