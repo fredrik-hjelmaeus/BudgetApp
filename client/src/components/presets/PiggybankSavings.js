11771ea116835aa8238dfccc4d20de3e191b2426
K <gemigpost@hotmail.com>
2021-02-15 12:24:22 +0100

    bugfix: trying to set purchase on null object

--- client/src/components/presets/PiggybankSavings.js
+++ client/src/components/presets/PiggybankSavings.js
@@ -6,17 +6,12 @@ const PiggybankSavings = () => {
   const presetContext = useContext(PresetContext);
   const { purchases } = presetContext;
   useEffect(() => {
-    presetContext.setPurchase();
+    // important to check if presets exists as this sideeffect is
+    // first to run in some occasions. For example when user press exit on guide in mobile version.
+    presetContext.presets && presetContext.setPurchase();
     // eslint-disable-next-line
   }, [presetContext.presets]);
 
-  return (
-    <Fragment>
-      {purchases &&
-        purchases.map((purchase) => (
-          <PiggyBankSavingItem key={purchase._id} purchase={purchase} />
-        ))}
-    </Fragment>
-  );
+  return <Fragment>{purchases && purchases.map((purchase) => <PiggyBankSavingItem key={purchase._id} purchase={purchase} />)}</Fragment>;
 };
 export default PiggybankSavings;
