c5a124ddf65bee66855d5ca60908f7bb6e8fb14f
K <gemigpost@hotmail.com>
2022-01-14 21:32:28 +0100

    cleanup

--- test/setup.js
+++ test/setup.js
@@ -1,30 +1,20 @@
-//const MongoMemoryServer = require('mongodb-memory-server');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const mongoose = require('mongoose');
-const app = require('../app');
 
 let mongo;
 // Setup hook for memory server
 beforeAll(async () => {
-  //<-- beforeAll is jest
-  //mongo = await MongoMemoryServer.create();
   mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
-  console.log(mongoUri);
+
   await mongoose.connect(mongoUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-
-  /*   await connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }); */
 });
 
 // Reset collections/data before testing
 beforeEach(async () => {
-  //<-- beforeEach is jest
   const collections = await mongoose.connection.db.collections();
 
   for (let collection of collections) {
@@ -34,9 +24,6 @@ beforeEach(async () => {
 
 // After testing is complete, stop the mongo memory server and tell mongoose to disconnect.
 afterAll(async () => {
-  //<-- afterAll is jest
   await mongo.stop();
   await mongoose.connection.close();
 });
-
-//module.exports = mongo;
