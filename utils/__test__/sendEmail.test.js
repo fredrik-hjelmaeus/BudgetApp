0f4931ff366964ce317cc0072cf72441beab8c75
K <gemigpost@hotmail.com>
2022-01-17 17:02:45 +0100

    auth tests and sendEmail test

---
+++ utils/__test__/sendEmail.test.js
@@ -0,0 +1,30 @@
+const sendEmail = require('../sendEmail');
+const config = require('config');
+const sendMailMock = jest.fn(); // this will return undefined if .sendMail() is called
+
+// In order to return a specific value you can use this instead
+// const sendMailMock = jest.fn().mockReturnValue(/* Whatever you would expect as return value */);
+
+jest.mock('nodemailer');
+
+const nodemailer = require('nodemailer');
+nodemailer.createTransport.mockReturnValue({ sendMail: sendMailMock });
+describe('sendEmail', () => {
+  afterEach(() => jest.clearAllMocks());
+  it('sends email with options', async () => {
+    await sendEmail({
+      email: 'test@test.com',
+      subject: 'Password reset token',
+      message: 'testing little',
+    });
+
+    expect(sendMailMock).toBeCalledTimes(1);
+    const sent = {
+      from: `${config.get('FROM_NAME')} <${config.get('FROM_EMAIL')}>`,
+      subject: 'Password reset token',
+      text: 'testing little',
+      to: 'test@test.com',
+    };
+    expect(sendMailMock).toHaveBeenCalledWith(sent);
+  });
+});
