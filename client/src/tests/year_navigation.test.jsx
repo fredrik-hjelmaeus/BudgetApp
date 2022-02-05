6e202600538152c2846cf24e7a329f9090f2c430
K <gemigpost@hotmail.com>
2022-02-06 00:36:41 +0100

    started logged in user tests

---
+++ client/src/tests/year_navigation.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from '../test-utils/context-wrapper';
+import App from '../App';
+
+describe('navigation through all year pages', () => {
+  test('initial state correct in year', () => {});
+  test('guide is activated on new user', () => {});
+  test('initial year state correct after exiting guide for new user', () => {});
+  test('initial year state correct after exiting guide for user with own presets created', () => {});
+  test('navigate to expense summary works', () => {});
+  test('navigate to income summary works', () => {});
+  test('navigate to savings summary works', () => {});
+  test('navigate back to balance summary works', () => {});
+  test('open user details modals works', () => {});
+  //render(<App />)
+});
