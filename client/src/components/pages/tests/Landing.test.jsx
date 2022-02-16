3122be0c30b64de8b99ed378ba05afe2b3dd2b7c
K <gemigpost@hotmail.com>
2022-02-16 20:11:43 +0100

    landing adjusted to new routing

--- client/src/components/pages/tests/Landing.test.jsx
+++ client/src/components/pages/tests/Landing.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen, fireEvent } from '../../../test-utils/context-wrapper';
 import Landing from '../Landing';
 import App from '../../../App';
+import { server } from '../../../mocks/server';
+import { rest } from 'msw';
 
 test('Landing page register button click activates register modal', () => {
   render(<Landing />);
@@ -24,6 +26,23 @@ test('forgot button click activates forgot modal', () => {
 });
 
 test('Landing is loaded when not authenticated', async () => {
+  // override endpoints so they do _not_ authenticate.
+  server.use(
+    // get current user via token endpoint
+    //fail to get user and will only try if token is found.
+    rest.get('http://localhost/api/auth', (req, res, ctx) => {
+      return res(
+        ctx.status(500),
+        ctx.json({
+          msg: 'No token, authorization denied',
+        })
+      );
+    }),
+    // get users presets
+    rest.get('http://localhost/api/userpreset', (req, res, ctx) => {
+      return res(ctx.status(401), ctx.json([]));
+    })
+  );
   render(<App />); //<- Create Virtual DOM
   const loginButton = await screen.findByRole('button', { name: /Login/i });
   const welcomeText = await screen.findByText(/An app that helps you organize your economy./i);
