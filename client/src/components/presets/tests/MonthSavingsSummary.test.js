f72dd926b58b776bef769a1e1addf4a484a9e0d3
K <gemigpost@hotmail.com>
2022-03-12 13:10:33 +0100

    test reorganisation

--- client/src/components/presets/tests/MonthSavingsSummary.test.js
+++ client/src/components/presets/tests/MonthSavingsSummary.test.js
@@ -6,8 +6,8 @@ import { server } from '../../../mocks/server';
 import { rest } from 'msw';
 import path from 'path';
 
-test('editing name and number on piggybank saving works correctly', async () => {});
-test('editing category on piggybank saving should not work', async () => {});
-test('deleting piggybank saving works correctly', async () => {});
-test('editing name,number and category on saving works correctly', async () => {});
-test('deleting saving works correctly', async () => {});
+test.skip('editing name and number on piggybank saving works correctly', async () => {});
+test.skip('editing category on piggybank saving should not work', async () => {});
+test.skip('deleting piggybank saving works correctly', async () => {});
+test.skip('editing name,number and category on saving works correctly', async () => {});
+test.skip('deleting saving works correctly', async () => {});
