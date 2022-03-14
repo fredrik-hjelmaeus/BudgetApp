f7d963ce8dd599412dfdcded1608a3c6c7b67cc7
K <gemigpost@hotmail.com>
2022-03-14 23:56:32 +0100

    editpiggybankmodal debug underway

--- client/src/context/css/CssState.js
+++ client/src/context/css/CssState.js
@@ -1,15 +1,9 @@
 import React, { useReducer } from 'react';
 import CssContext from './cssContext';
 import cssReducer from './cssReducer';
-import {
-  HIDE_NAVBAR,
-  TOGGLE_MODAL,
-  SET_YEARSUMMARY,
-  SET_MODAL_PROPS,
-  SET_DIMENSIONS
-} from '../types';
+import { HIDE_NAVBAR, TOGGLE_MODAL, SET_YEARSUMMARY, SET_MODAL_PROPS, SET_DIMENSIONS } from '../types';
 
-const CssState = props => {
+const CssState = (props) => {
   const initialState = {
     navbar: true,
     modal: '',
@@ -17,25 +11,23 @@ const CssState = props => {
     yearsummary: 'balance',
     dimensions: {
       height: window.innerHeight,
-      width: window.innerWidth
-    }
+      width: window.innerWidth,
+    },
   };
 
   const [state, dispatch] = useReducer(cssReducer, initialState);
 
   // Toggle Navbar
-  const toggleNavbar = navbar =>
-    dispatch({ type: HIDE_NAVBAR, payload: navbar });
+  const toggleNavbar = (navbar) => dispatch({ type: HIDE_NAVBAR, payload: navbar });
 
   // Toggle modal
-  const toggleModal = modal => dispatch({ type: TOGGLE_MODAL, payload: modal });
+  const toggleModal = (modal) => dispatch({ type: TOGGLE_MODAL, payload: modal });
 
   // set modal props
-  const setModalprops = what =>
-    dispatch({ type: SET_MODAL_PROPS, payload: what });
+  const setModalprops = (props) => dispatch({ type: SET_MODAL_PROPS, payload: props });
 
   // Set Yearsummary
-  const setYearSummary = yearsummary => {
+  const setYearSummary = (yearsummary) => {
     dispatch({ type: SET_YEARSUMMARY, payload: yearsummary });
   };
 
@@ -56,7 +48,7 @@ const CssState = props => {
         toggleModal,
         setYearSummary,
         setModalprops,
-        setDimensions
+        setDimensions,
       }}
     >
       {props.children}
