ff392fdd2e84fca8a60e304437cc4a9937a906ef
K <gemigpost@hotmail.com>
2022-03-23 15:46:08 +0100

    debug cleanup

--- client/src/components/presets/EditPiggybankModal.js
+++ client/src/components/presets/EditPiggybankModal.js
@@ -1,5 +1,4 @@
 import React, { useContext, useState, useEffect } from 'react';
-import uuid from 'uuid';
 import PresetContext from '../../context/preset/presetContext';
 import CssContext from '../../context/css/cssContext';
 import PiggybankSVG from '../layout/images/PiggybankSVG';
@@ -25,27 +24,9 @@ const EditPiggybankModal = ({ Item }) => {
     piggybank: Item.piggybank,
   });
 
-  let AmountToSave; //init startvalue
-
-  // store only savedAmounts in an array
-  const savedAmounts = Item.piggybank.map((item) => item.savedAmount);
-
   const monthPiggyPresets = Item.piggybank.filter((item) => item.month === presetContext.month);
   const savedMonthsAmounts = monthPiggyPresets.map((p) => p.savedAmount);
   const piggySum = savedMonthsAmounts.reduce((a, b) => a + b, 0);
-  // console.log(MonthSum);
-  //  console.log(piggySum);
-
-  // sift through savedAmounts and count totalsum
-  const SumOfPiggybanks = savedAmounts.reduce((a, b) => parseFloat(a) + parseFloat(b), 0);
-  const SumLeftToSave = parseFloat(Item.number) - parseFloat(SumOfPiggybanks);
-
-  // Calc Amount to save
-  /*   if (parseInt(MonthBalance) > parseInt(SumLeftToSave)) {
-    AmountToSave = SumLeftToSave;
-  } else {
-    AmountToSave = MonthBalance;
-  } */
 
   //piggybankstate, local state used for onChange-slider
   const [piggybank, setPiggybank] = useState({ number: piggySum });
@@ -72,48 +53,12 @@ const EditPiggybankModal = ({ Item }) => {
       month: presetContext.month,
       year: parseInt(presetContext.year),
       savedAmount: parseInt(piggybank.number),
-      //  _id: uuid.v4(),
     });
-    // console.log(presetContext.piggybanks);
-    // console.log(monthPiggyPresets);
+
     await setActivePiggybank(monthPiggyPresets);
-    //console.log("piggybanks before sendmyedit", piggybanks);
-    /*     sendMyEdit({
-      _id: Item._id,
-      name: Item.name,
-      number: Item.number,
-      month: Item.month,
-      year: Item.year,
-      category: Item.category,
-      type: "purchase",
-      piggybank: piggybanks,
-    }); */
-    // setPreset({ ...preset, piggybank: piggybanks });
-    //await sendEdit(preset);
-    //  setActivePiggybank(monthPiggyPresets);
-    // add new piggybank with value(piggybank.number)
-    /*    addtoPiggybanks({
-      month: presetContext.month,
-      year: presetContext.year,
-      savedAmount: parseInt(piggybank.number),
-    }); */
-
-    //console.log('piggybank.number', piggybank.number);
-    // console.log('piggybanks', piggybanks);
-    //   console.log('addtopiggybanks ran');
-
-    //  console.log('onsubmit preset:', preset);
   };
   useEffect(() => {
-    /*     console.log(
-      "objs:",
-      piggybanks,
-      modalprops.piggybank,
-      piggybanks === modalprops.piggybank
-    ); */
     if (piggybanks.length !== 0 && piggybanks !== modalprops.piggybank) {
-      //  console.log("piggybanks change detected");
-      // console.log(piggybanks);
       sendMyEdit({
         _id: Item._id,
         name: Item.name,
@@ -127,49 +72,12 @@ const EditPiggybankModal = ({ Item }) => {
     }
   }, [piggybanks, modalprops.piggybank]);
 
-  // on presetContext.piggybanks change,set local preset with new presetContext.piggybanks-value.
-  /* useEffect(() => {
-    console.log("piggybanks check:");
-    console.log(piggybanks.length, modalprops.piggybank.length);
-    if (
-      presetContext.piggybanks.length !== undefined &&
-      presetContext.piggybanks.length !== 0
-      //    presetContext.piggybanks.length !== modalprops.piggybank.length
-    ) {
-      console.log("useEffect setPreset");
-      console.log("piggybanks:", presetContext.piggybanks);
-      setPreset({
-        ...preset,
-        _id: Item._id,
-        name: Item.name,
-        number: Item.number,
-        month: Item.month,
-        year: Item.year,
-        category: Item.category,
-        type: "purchase",
-        piggybank: presetContext.piggybanks,
-      });
-    } // eslint-disable-next-line
-  }, [presetContext.piggybanks, modalprops.piggybank.length]); */
-
   // on sending preset to database,wait and then close modal first then reset/unload presetContext.piggybanks
   const sendMyEdit = async (preset) => {
     await sendEdit(preset);
     toggleModal('');
     setActivePiggybank([]);
   };
-
-  // on local preset change, push new preset to database
-  /* useEffect(() => {
-    if (
-      presetContext.piggybanks.length !== undefined &&
-      presetContext.piggybanks.length !== 0
-      // presetContext.piggybanks.length !== modalprops.piggybank.length
-    ) {
-      console.log("useEffect sendMyEdit");
-      sendMyEdit(preset);
-    } // eslint-disable-next-line
-  }, [preset]); */
 
   // Close modal
   const onClick = (e) => {
