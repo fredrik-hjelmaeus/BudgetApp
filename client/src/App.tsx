fe1bcd9282102778f242c1718bf624dcaf7db5ac
K_on_Mac <gemigpost@hotmail.com>
2022-04-15 21:37:47 +0200

    css context refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,22 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 
-import PrivateRoute from './components/routing/PrivateRoute';
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-import Landing from './components/pages/Landing';
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import ResetPassword from './components/auth/ResetPassword';
-import PresetState from './context/preset/PresetState';
-import AuthState from './context/auth/AuthState';
-import AlertState from './context/alert/AlertState';
-import CssState from './context/css/CssState';
-import GuideState from './context/guide/GuideState';
-import DateState from './context/date/DateState';
-import setAuthToken from './utils/setAuthToken';
+import PrivateRoute from "./components/routing/PrivateRoute";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Landing from "./components/pages/Landing";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import ResetPassword from "./components/auth/ResetPassword";
+import PresetState from "./context/preset/PresetState";
+import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
+import CssState from "./context/css/CssState";
+
+import GuideState from "./context/guide/GuideState";
+import DateState from "./context/date/DateState";
+import setAuthToken from "./utils/setAuthToken";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -34,10 +35,10 @@ const App = () => {
                     <Navbar />
                     <div>
                       <Switch>
-                        <PrivateRoute exact path='/' component={Home} />
-                        <Route exact path='/Landing' component={Landing} />
-                        <Route exact path='/about' component={About} />
-                        <Route path='/resetpassword/:id' component={ResetPassword} />
+                        <PrivateRoute exact path="/" component={Home} />
+                        <Route exact path="/Landing" component={Landing} />
+                        <Route exact path="/about" component={About} />
+                        <Route path="/resetpassword/:id" component={ResetPassword} />
                       </Switch>
                     </div>
                     <Footer />
