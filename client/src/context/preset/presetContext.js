b1509f0c06c2eef9847164f01f490ee77a6b78f6
K_on_Mac <gemigpost@hotmail.com>
2022-04-21 19:59:18 +0200

    presetState to ts stable

--- client/src/context/preset/presetContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const presetContext = createContext();
-
-export default presetContext;
--- client/src/frontend-types/IPresetContext.ts
+++ client/src/frontend-types/IPresetContext.ts
@@ -1,10 +1,11 @@
 import { ICategoryAndSumItem } from "./ICategoryAndSumItem";
 import { IPiggybank } from "./IPiggybank";
 import { IPreset } from "./IPreset";
+import { IUploadCsv } from "./IUploadCsv";
 
 export interface IPresetState {
   presets: null | IPreset[];
-  sum: null | number; // sum of all presets
+  /*  sum: null | number; // sum of all presets
   edit: null | IPreset;
   error: null | string; // TODO: change to string[]
   month: null | string; // "January"
@@ -26,15 +27,60 @@ export interface IPresetState {
   categorysumonlynegnumbyyear: null | number[]; // used in donut char, setter fn name is calcCategorySumOnlyNegNumByYear
   piggybanks: IPiggybank[]; // used when editing piggybanks on one preset. Holds the edit values.
   monthsavings: null | number; // holds savings by month
-  monthsavingspresets: null | IPreset[]; // presets with savings added this month and purchases with piggybanksavings added this month
-  monthpiggysavings: null; // year implemented no legacy   <------- HERE
-  SumPiggybanksMonth: 0; // year implemented
-  MonthBalance: null; // year implemented
-  purchases: null; // year independent
-  csvpresets: null; // used to store values from csv-file in stagingarea
-  doSubmitCsv: "";
-  savingsList: [];
-  capitalList: [];
+  monthsavingspresets: null | IPreset[]; // presets with savings added this month , setter: getMonthSavings
+  monthpiggysavings: null | IPreset[]; // purchase presets with piggybanksavings added this month,setter: getMonthPiggySavings
+  SumPiggybanksMonth: number; //  Sum of all piggybanks in current/active month, setter: setTotalOfAllPiggybanksThisMonth
+  MonthBalance: null | number; // Sum of MonthSum-monthsavings-SumPiggybanksMonth, setter:calcMonthBalance
+  purchases: null | IPreset[]; // list of all purchases
+  csvpresets: null | IPreset[]; // used to store preset values from csv-file in stagingarea
+  doSubmitCsv: string;
+  savingsList: IPreset[]; // List of savings, setter: getSavingsList
+  capitalList: IPreset[]; // List of capital, setter: getCapitalList */
 }
 
-export interface IPresetContext extends IPresetState {}
+export interface IPresetContext extends IPresetState {
+  addPreset(preset: IPreset): void;
+  getPresets(): Promise<void>;
+  /* calcSum(): void;
+  deletePreset(id: string): Promise<void>;
+  setEdit(preset: IPreset): void;
+  cancelEdit(): void;
+  sendEdit(preset: IPreset): Promise<void>; // Should be named updatePreset ?. It sends the edit-preset.
+  
+  clearPresets(): void;
+  addMonth(month: string): void;
+  calcMonthSum(month: string): void;
+  filterOutPositiveNumsAndMonth(month: string): void;
+  filterOutNegativeNumsAndMonth(month: string): void;
+  calcPosMonth(): void;
+  calcNegMonth(): void;
+  calcCategoryByMonth(month: string): void;
+  calcCategoryByYear(): void;
+  resetSums(): void;
+  setYear(year: number): void;
+  calcYearsum(year: number): void;
+  calcSavings(): void;
+  calcCapital(): void;
+  setPurchase(): void;
+  setCategoryNameOnlyPosNumByYear(): void;
+  setCategoryNameOnlyNegNumByYear(): void;
+  calcCategorySumOnlyPosNumByYear(): void;
+  calcCategorySumOnlyNegNumByYear(): void;
+  calcAllMonthSum(montharray: string[]): void;
+  addtoPiggybanks(object: IPiggybank): void;
+  setActivePiggybank(piggybankArray: IPiggybank[]): void;
+  clearPiggybanks(): void;
+  calcMonthSavings(): void;
+  getMonthSavings(month: string): void;
+  getMonthPiggySavings(): void;
+  setTotalOfAllPiggybanksThisMonth(Sum: number): void;
+  calcMonthBalance(): void;
+  uploadCSV(formData: IUploadCsv): Promise<void>;
+  getSavingsList(): void;
+  getCapitalList(): void;
+  updateCsvPresets(preset: IPreset): void;
+  removeCSV(preset: IPreset): void;
+  clearCsv(): void;
+  submitCsvItems(string: string): void;
+  getGuidePresets(): void; */
+}
