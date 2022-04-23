57532ce6592cc11a6613e96b250bc9458b246bc5
K_on_Mac <gemigpost@hotmail.com>
2022-04-23 14:33:44 +0200

    alertcontext to ts complet

--- client/src/context/alert/AlertState.tsx
+++ client/src/context/alert/AlertState.tsx
@@ -1,16 +1,23 @@
-import React, { useReducer } from "react";
+import React, { ReactNode, useReducer } from "react";
 import uuid from "uuid";
 import AlertContext from "./alertContext";
 import alertReducer from "./alertReducer";
 import { SET_ALERT, REMOVE_ALERT } from "../types";
 
-const AlertState = (props) => {
-  const initialState = [];
+export interface alertObject {
+  msg: string;
+  type: string;
+  id: string;
+}
+type setAlert = (msg: string, type: string, timeout?: number) => void;
+
+const AlertState = (props: { children: ReactNode }) => {
+  const initialState: alertObject[] = [];
 
   const [state, dispatch] = useReducer(alertReducer, initialState);
 
   // Set Alert
-  const setAlert = (msg, type, timeout = 5000) => {
+  const setAlert: setAlert = (msg, type, timeout = 5000) => {
     const id = uuid.v4();
 
     dispatch({
