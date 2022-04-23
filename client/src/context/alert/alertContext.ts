59f811838042c05cc8a37ba84e7f6357f4c90f48
K_on_Mac <gemigpost@hotmail.com>
2022-04-23 16:58:39 +0200

    alertcontext to ts complete

--- client/src/context/alert/alertContext.ts
+++ client/src/context/alert/alertContext.ts
@@ -1,5 +1,6 @@
-import { createContext } from 'react';
+import { createContext } from "react";
+import { IAlertContext } from "../../frontend-types/IAlertContext";
 
-const alertContext = createContext();
+const alertContext = createContext<IAlertContext | null>(null);
 
 export default alertContext;
--- client/src/context/alert/alertReducer.ts
+++ client/src/context/alert/alertReducer.ts
@@ -1,16 +1,22 @@
+import { alertObject, IAlertState } from "../../frontend-types/IAlertContext";
 import { SET_ALERT, REMOVE_ALERT } from "../types";
-import { alertObject } from "./AlertState";
 
 type ActionType =
   | { type: typeof SET_ALERT; payload: alertObject }
   | { type: typeof REMOVE_ALERT; payload: string };
 
-function alertReducer(state: alertObject[], action: ActionType) {
+function alertReducer(state: IAlertState, action: ActionType) {
   switch (action.type) {
     case SET_ALERT:
-      return [...state, action.payload];
+      return {
+        ...state,
+        alerts: [...state.alerts, action.payload],
+      };
     case REMOVE_ALERT:
-      return state.filter((alert) => alert.id !== action.payload);
+      return {
+        ...state,
+        alerts: state.alerts.filter((alert) => alert.id !== action.payload),
+      };
     default:
       return state;
   }
