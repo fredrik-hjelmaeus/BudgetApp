46927044bf51f8bf59e80bd824aa5176d87781b1
K_on_Mac <gemigpost@hotmail.com>
2022-04-25 12:33:37 +0200

    upgrade to react-router-dom v6

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import PrivateRoute from "./components/routing/PrivateRoute";
@@ -35,19 +35,12 @@ const App = () => {
                   <Fragment>
                     <Navbar />
                     <div>
-                      <Switch>
-                        {/* removed exact from all routes, see:
-                        https://stackoverflow.com/questions/69866581/property-exact-does-not-exist-on-type */}
-                        {/*  <Route path="/" element={<PrivateRoute component={Home} />} />
+                      <Routes>
+                        <Route path="/" element={<PrivateRoute component={Home} />} />
                         <Route path="about" element={<About />} />
                         <Route path="Landing" element={<Landing />} />
-                        <Route path="resetpassword/:id" element={<ResetPassword />} /> */}
-                        <PrivateRoute exact path="/" component={Home} />
-                        {/*  <PrivateRoute path="/" component={Home} /> */}
-                        <Route path="/Landing" component={Landing} />
-                        <Route path="/about" component={About} />
-                        <Route path="/resetpassword/:id" component={ResetPassword} />
-                      </Switch>
+                        <Route path="resetpassword/:id" element={<ResetPassword />} />
+                      </Routes>
                     </div>
                     <Footer />
                   </Fragment>
