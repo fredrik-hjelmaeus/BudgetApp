339055c12f6fd5bb4de6951dcc03f05f6d488032
K_on_Mac <gemigpost@hotmail.com>
2022-05-05 21:44:53 +0200

    dateitemmobile to ts

--- client/src/context/date/dateContext.ts
+++ client/src/context/date/dateContext.ts
@@ -1,6 +1,9 @@
 import { createContext } from "react";
 import { IDateContext } from "../../frontend-types/IDateContext";
 
-const dataContext = createContext<IDateContext | null>(null);
+const dataContext = createContext<IDateContext>({
+  dateList: [],
+  setDate: (dateList) => {},
+});
 
 export default dataContext;
