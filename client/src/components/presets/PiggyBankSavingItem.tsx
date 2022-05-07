58b51eacce9a0651265b267a1702d058197db153
K_on_Mac <gemigpost@hotmail.com>
2022-05-07 13:25:03 +0200

    piggyitem piggybanksavingitem and piggybanksavings to ts

--- client/src/components/presets/PiggyBankSavingItem.tsx
+++ client/src/components/presets/PiggyBankSavingItem.tsx
@@ -1,34 +1,47 @@
 import React, { useContext, useState } from "react";
-import piggyicon from "../layout/images/piggybank.svg";
+//import piggyicon from "../layout/images/piggybank.svg";
 import PresetContext from "../../context/preset/presetContext";
+import AlertContext from "../../context/alert/alertContext";
 import PiggyItem from "./PiggyItem";
+import PiggybankSVG from "../layout/images/PiggybankSVG";
+import { IPreset } from "../../frontend-types/IPreset";
 
-const PiggyBankSavingItem = ({ purchase }) => {
+interface PiggyBankSavingItemProps {
+  preset: IPreset;
+}
+
+const PiggyBankSavingItem = ({ preset }: PiggyBankSavingItemProps) => {
   const presetContext = useContext(PresetContext);
   const { year, sendEdit } = presetContext;
-  const { name, number, piggybank, _id } = purchase;
+  const alertContext = useContext(AlertContext);
+  const { setAlert } = alertContext;
+
+  const { name, number, piggybank } = preset;
 
   /* Look witch year it is and filter out years above this year and savedAmounts that is zero */
-  const filterpiggybanks = piggybank.filter(
-    (item, index) => item.year && item.year <= year && index !== 0
-  );
+  const filterpiggybanks =
+    year && piggybank.filter((item, index) => item.year && item.year <= year && index !== 0);
 
   /*take the filtered yearlist and create a var for the sums saved */
-  const sumoffilteredpiggybanks = filterpiggybanks.map((item) => item.savedAmount);
+  const sumoffilteredpiggybanks =
+    filterpiggybanks !== 0 && filterpiggybanks?.map((item) => item.savedAmount);
   /* sum up the amounts */
-  const totalsum = sumoffilteredpiggybanks.reduce((a, b) => parseFloat(a) + parseFloat(b), 0);
+  const totalsum = sumoffilteredpiggybanks && sumoffilteredpiggybanks.reduce((a, b) => a + b, 0);
 
   const [showIndividualPiggyItems, setShowIndividualPiggyItems] = useState(false);
   const onClick = () => {
     setShowIndividualPiggyItems(!showIndividualPiggyItems);
   };
 
-  const onPiggyItemDelete = (piggyItemId) => {
+  const onPiggyItemDelete = (piggyItemId: string | undefined) => {
+    if (piggyItemId === undefined) {
+      setAlert("No id found for this piggybank item!", "danger");
+    }
     // construct new piggybank-list where provided id is filtered out.
-    const newPiggybankList = piggybank.filter((piggyItem) => piggyItem._id !== piggyItemId);
+    const newPiggybankList = piggybank.filter((piggyItem) => piggyItem.id !== piggyItemId);
 
     // add new piggybank to deconstructed preset
-    const newPreset = { ...purchase, piggybank: newPiggybankList };
+    const newPreset = { ...preset, piggybank: newPiggybankList };
 
     // update preset using _id
     sendEdit(newPreset);
@@ -41,7 +54,11 @@ const PiggyBankSavingItem = ({ purchase }) => {
           {name}
         </div>
         <div className="flexrow-piggycard">
-          <img src={piggyicon} alt="" style={{ width: "26px" }} />
+          <div>
+            <PiggybankSVG fill="var(--gray-color)" />{" "}
+            {/* TODO: confirm this is working and remove below */}
+            {/*    <img src={piggyicon} alt="" style={{ width: "26px" }} /> */}
+          </div>
           <div className="px text-orange expandbutton">{totalsum}</div>
           <div className="text-light">of</div>
           <div className="text-gray px">({number})</div>
@@ -53,7 +70,7 @@ const PiggyBankSavingItem = ({ purchase }) => {
           <PiggyItem
             onPiggyItemDelete={onPiggyItemDelete}
             piggyItem={piggyItem}
-            key={piggyItem._id}
+            key={piggyItem.id}
           />
         ))}
     </>
