a6c7a4de0952db0d675371f3eb43e9fc51c1c87f
K_on_Mac <gemigpost@hotmail.com>
2022-05-08 14:14:46 +0200

    last components converted to ts

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -1,55 +1,64 @@
-import React from 'react';
-import instagramicon from './images/instagramicon.svg';
-import twittericon from './images/twittericon.svg';
-import facebookicon from './images/facebookicon.svg';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import FacebookIconSVG from "./images/facebookicon"; // TODO: converted to SVG and styles may not work
+import TwitterIconSVG from "./images/twittericon"; // TODO: converted to SVG and styles may not work
+import InstagramIconSVG from "./images/instagramicon"; // TODO: converted to SVG and styles may not work
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-      <div className='footer__cols'>
-        <li className='footer__contact'>CONTACT</li>
+    <footer className="footer">
+      <div className="footer__cols">
+        <li className="footer__contact">CONTACT</li>
         <li>
-          <button className='footer__contact_btn'>+46 072 515 16 03</button>
+          <button className="footer__contact_btn">+46 072 515 16 03</button>
           <br />
-          <button className='footer__contact_btn'>gemigpost@hotmail.com</button>
+          <button className="footer__contact_btn">gemigpost@hotmail.com</button>
         </li>
         <li></li>
-        <li className='footer__service'>SERVICES</li>
+        <li className="footer__service">SERVICES</li>
         <li>
-          <button className='footer__service_btn'>Guide</button>
+          <button className="footer__service_btn">Guide</button>
           <br />
-          <button className='footer__service_btn'>FAQ</button>
+          <button className="footer__service_btn">FAQ</button>
         </li>
         <li></li>
 
-        <li className='footer__information'>INFORMATION</li>
+        <li className="footer__information">INFORMATION</li>
         <li>
-          <Link to='/about' className='footer__information_btn'>
+          <Link to="/about" className="footer__information_btn">
             About
           </Link>
 
           <br />
-          <button className='footer__information_btn'>Work with me</button>
+          <button className="footer__information_btn">Work with me</button>
           <br />
-          <button className='footer__information_btn'>Policy privacy</button>
+          <button className="footer__information_btn">Policy privacy</button>
           <br />
-          <button className='footer__information_btn'>Terms & Conditions</button>
+          <button className="footer__information_btn">Terms & Conditions</button>
           <br />
-          <button className='footer__information_btn'>Press Enquires</button>
+          <button className="footer__information_btn">Press Enquires</button>
         </li>
         <li></li>
-        <li className='footer__links'>
-          <a href='https://www.instagram.com/leomessi/' className='footer__links_btn'>
-            <img src={instagramicon} alt='img' className='footericons' />
+        <li className="footer__links">
+          <a href="https://www.instagram.com/leomessi/" className="footer__links_btn">
+            <div className="footericons">
+              <InstagramIconSVG />
+            </div>
+            {/* <img src={instagramicon} alt="img" className="footericons" /> */}
           </a>
 
-          {'     '}
-          <a href='https://twitter.com/imessi' className='footer__links_btn'>
-            <img src={twittericon} alt='img' className='footericons' />
+          {"     "}
+          <a href="https://twitter.com/imessi" className="footer__links_btn">
+            <div className="footericons">
+              <TwitterIconSVG />
+            </div>
+            {/*   <img src={twittericon} alt="img" className="footericons" /> */}
           </a>
-          <a href='https://www.facebook.com/leomessi/' className='footer__links_btn'>
-            <img src={facebookicon} alt='img' className='footericons' />
+          <a href="https://www.facebook.com/leomessi/" className="footer__links_btn">
+            <div className="footericons">
+              <FacebookIconSVG />
+            </div>
+            {/*  <img src={facebookicon} alt="img" className="footericons" /> */}
           </a>
         </li>
         <li></li>
