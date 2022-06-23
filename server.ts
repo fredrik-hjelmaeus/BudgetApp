e2e4bc0bb3791d33374dca29e946d40d99f49fd6
K_on_Mac <gemigpost@hotmail.com>
2022-06-23 17:59:23 +0200

    prod fix 11

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === "production") {
 
   //app.get("/*", (req, res) => res.send("404"));
   app.get("/*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "public", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
 
