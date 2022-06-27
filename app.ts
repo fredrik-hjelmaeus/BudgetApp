30c69f0a57cc53296ae6c8f9b898091cafbf87cd
K_on_Mac <gemigpost@hotmail.com>
2022-06-27 16:09:36 +0200

    routes change and homepage url change in package.json

--- app.ts
+++ app.ts
@@ -120,13 +120,13 @@ app.use("/api/userpreset/upload", require("./routes/upload"));
 app.use("/api/guide", require("./routes/guide"));
 
 //production mode
-if (process.env.NODE_ENV === "production") {
+/* if (process.env.NODE_ENV === "production") {
   const productionPath = path.join(__dirname, "../", "client", "build", "index.html");
   app.get("/*", (req, res) => {
     console.log("catchallpath detected");
     res.sendFile(productionPath);
   });
-}
+} */
 
 /* app.get(
   "/*",
